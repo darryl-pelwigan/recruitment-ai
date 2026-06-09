0ac0c0fcd050187f7cca44d3cf531f3eec45a1a3
Darryl Pelwigan <darryl.pelwigan@gmail.com>
2026-06-09 15:47:37 +0800

    clean up code - check if ui and api is communicating

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import engine, Base
 
 app = FastAPI(title="Recruitment AI System", version="1.0")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
+
+@app.get("/api/")
 def root():
     return {"message": "Welcome to the Recruitment AI System!"}
